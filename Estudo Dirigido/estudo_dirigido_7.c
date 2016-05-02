03e2c30f29387ce4460d70359e3d54f3cc6a5a12
Rafael Dias <rafa197795@gmail.com>
2016-05-02 14:57:01 -0300

    Ajuste do bug no estudo dirigido 7

--- Estudo Dirigido/estudo_dirigido_7.c
+++ Estudo Dirigido/estudo_dirigido_7.c
@@ -7,35 +7,45 @@
 
 #include <zconf.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define USUARIOS 5
+#define USUARIOS 2
 #define N 30
 
 int cartas = 0;
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-pthread_cond_t cond_pombo = PTHREAD_COND_INITIALIZER;;
-pthread_cond_t cond_user = PTHREAD_COND_INITIALIZER;;
+pthread_cond_t cond_pombo = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_user = PTHREAD_COND_INITIALIZER;
 
 void* pombo() {
-    pthread_mutex_lock(&lock);
-    if (cartas == N) {
+    while(1) {
+        pthread_mutex_lock(&lock);
+        if (cartas >= 0) {
+            printf("Vai dar o wait no pombo\n");
+            pthread_cond_wait(&cond_pombo, &lock);
+        }
         cartas = 0;
+        printf("Valor de cartas após o pombo: %d\n", cartas);
+        sleep(3);
         pthread_cond_broadcast(&cond_user);
-        pthread_cond_wait(&cond_pombo, &lock);
+        pthread_mutex_unlock(&lock);
     }
-    pthread_mutex_unlock(&lock);
 }
 
 void* user() {
-    pthread_mutex_lock(&lock);
-    if (cartas == N) {
-        pthread_cond_signal(&cond_pombo);
-        pthread_cond_wait(&cond_user, &lock);
+    while(1) {
+        pthread_mutex_lock(&lock);
+        if (cartas >= N) {
+            printf("Entrou no if do user com %d cartas\n", cartas);
+            pthread_cond_broadcast(&cond_pombo);
+            pthread_cond_wait(&cond_user, &lock);
+        }
+        cartas++;
+        pthread_mutex_unlock(&lock);
     }
-    cartas++;
-    pthread_mutex_unlock(&lock);
 }
 
 int main() {
@@ -49,10 +59,10 @@ int main() {
         pthread_create(&users[i], NULL, user, NULL);
     }
 
-    pthread_join(&pombos, NULL);
+    pthread_join(pombos, NULL);
 
     for (int i = 0; i < USUARIOS; ++i) {
-        pthread_join(&users[i], NULL);
+        pthread_join(users[i], NULL);
     }
 
     return 0;
